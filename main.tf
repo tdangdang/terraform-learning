6a769752d5a42deaae9e87bac93251358d4eddfd
tdangdang <gtang@DESKTOP-F8MBN00.tcp.com>
2022-01-16 16:57:13 -0500

    add ec2 deployment

--- main.tf
+++ main.tf
@@ -2,58 +2,124 @@ provider "aws"  {
     region = "us-east-1"
 }
 
-variable "cidr_blocks" {
-    description = "scidr blocks and tag names for vpc and subnets"
-    type = list(object({
-        cidr_block = string 
-        name = string
-        
-    }))
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable pubic_key_location {}
 
 
-resource "aws_vpc" "dev" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags ={
-        Name: var.cidr_blocks[0].name
+        Name: "${var.env_prefix}-vpc"
     }
 
 }
 
-resource "aws_subnet" "dev-subnet-1"  {
-    vpc_id = aws_vpc.dev.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "us-east-1a"
+resource "aws_subnet" "myapp-subnet-1"  {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags ={
-        Name: var.cidr_blocks[1].name
-    }
-}
-
-data "aws_vpc" "existing_vpc" {
-    default = true
-}
-
-resource "aws_subnet" "dev-subnet-2"  {
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = "172.31.96.0/20"
-    availability_zone = "us-east-1a"
-    tags ={
-        Name: "subnet-default-dev"
+        Name: "${var.env_prefix}-subnet-1"
     }
 }
 
 
-output "dev-vpc-id" {
-  value       = aws_vpc.dev.id
-  description = "DEV VPC ID"
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
+
 }
 
 
-output "dev-subnet" {
-  value       = aws_subnet.dev-subnet-1.id
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
+}
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-default-sg"
+    }
+
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+    }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
 }
 
 
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.pubic_key_location)
+}
 
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = file("entry-script.sh")
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
+
+}
 
 
